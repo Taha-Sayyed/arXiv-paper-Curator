bf09ce85f6a1f3b4f25716a54676a985ae8de2d6
Taha Sayyed <codetaha786@gmail.com>
2025-11-29 17:35:44 +0530

    chore: import required modules in docling.py

---
+++ src/services/pdf_parser/docling.py
@@ -0,0 +1,16 @@
+#by taha
+
+import logging
+import pypdfium2 as pdfium
+
+
+from pathlib import Path
+from typing import Optional
+from docling.datamodel.base_models import InputFormat
+from docling.datamodel.pipeline_options import PdfPipelineOptions
+from docling.document_converter import DocumentConverter, PdfFormatOption
+from src.exceptions import PDFParsingException, PDFValidationError
+from src.schemas.pdf_parser.models import PaperFigure, PaperSection, PaperTable, ParserType, PdfContent
+
+
+logger = logging.getLogger(__name__)
